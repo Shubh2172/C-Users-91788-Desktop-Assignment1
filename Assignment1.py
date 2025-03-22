56b4886cff5913a7691433efae27366a90118106
Shubh2172 <1shubhampatial444@gmail.com>
2025-03-22 18:44:07 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,99 @@
+# # string = "Shubham"
+# # list = [1,2,3,"Shubham",True,'Patial',6.5 ]
+# # float = 88.88
+# # a = (1,2,3,4,'shubham',True)
+
+# # print(string)
+# # print(type(list))
+# # print(type(a))
+# # print(type(float))
+
+# # var1 = " "
+# # var2 = [ 'DS', 'ML' , 'Python']
+# # var3 = [ "DS" , "ML" , "Python" ]
+# # var4 = 1.
+
+# # print(type(var1))
+# # print(type(var2))
+# # print(type(var3))
+# # print(type(var4))
+
+# # a = 5
+# # b = 8
+
+# # c1 = a / b
+# # c2 = a % b
+# # c3 = a // b
+# # c4 = a ** b
+# # print(c1)
+# # print(c2)
+# # print(c3)
+# # print(c4)
+
+# # a = [1,2,'shubham',True,4.3,[1,1,3,4],(4,5,3,5),{3,5,3}]
+
+# # for i in a :
+# #     print(type(i))
+# # else:
+# #     print("end")
+
+
+# # n = int(input("enter by user"))
+
+# # i = 8
+
+# # while  i/n:
+# #     print(i)
+# #     i = i + 8
+# #     break
+# # else:
+# #     print("end")
+
+# # # n = 4
+# # # i=1
+
+# # # while i < n:
+# # #     print(i)
+# # #     i = i + 1
+# # #     if i == 3 :
+# # #         break
+# # # else :
+# # #     print("this is my end")
+
+
+# list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+
+# i = 3
+# for i in list:
+#     print(list)
+#     i = i / 3
+
+# # if list / 3:
+# #     print("yes")
+# else:
+#     print("end")
+
+
+# string is immutable
+
+# but list it is
+# x = 5
+
+# y = 10
+
+# if x > y:
+
+#     print("x is greater")
+
+# else:
+
+#     print("y is greater")
+
+
+count = 0
+
+while count < 5:
+    count += 1
+    print(count)
+
+    
