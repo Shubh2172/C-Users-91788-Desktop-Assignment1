c108f69806b8437bee0ca0cee5179992e1dc54e9
Shubh2172 <1shubhampatial444@gmail.com>
2025-03-22 19:03:36 +0530

    Second Assignment Completed

---
+++ Assignment2.py
@@ -0,0 +1,44 @@
+# Q1 How do you comment code in Pyhton> What are the diiferent types of comments?
+# Ans - THis is comments
+
+# Ans of Second QUe
+# Variable is which area wherre we store the Data 
+# a = 10
+# 10 is data which store in a. a is variable
+
+# THird answer
+a = 10
+print(type(a))
+x = float(a)
+print(type(x))
+
+# ans 4  use the command "C:\Users\91788/Assignment2.py"
+
+# ans 5 
+list = [1,2,3,4,5]
+print(list[1:3])
+
+# ans 6 - Complex number is in mathematich combination of two numer one is real and second in imaginary number.
+a = 5 + 6j
+print(type(a))
+print(a.real)
+print(a.imag)
+
+# ans 8
+age = 25    #DEclare
+print(age)
+
+# Ans 9
+price = 9.99 
+print(type(price))
+
+#anss 10
+a = "Hello World"
+print(a[6:])
+
+#ans 11
+
+is_student = True
+print(is_student)
+
+
